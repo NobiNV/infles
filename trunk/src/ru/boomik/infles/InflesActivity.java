382e491b14a37f4170900f186fbecd2401e8d8a7
boom.vrn@gmail.com <boom.vrn@gmail.com@54e350b6-4f17-5e9d-aadd-4ef110a2a76b>
2011-07-05 20:29:04 +0000

    -change license to MIT -clean source

--- trunk/src/ru/boomik/infles/InflesActivity.java
+++ trunk/src/ru/boomik/infles/InflesActivity.java
@@ -1,22 +1,26 @@
 /* InflesActivity.java
  *
  * Copyright 2011 Kirill Ashikhmin
- * All Rights Reserved.
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
+ * Copyright (c) <year> <copyright holders>
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ * The above copyright notice and this permission notice shall be included 
+ * in all copies or substantial portions of the Software.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, 
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS 
+ * OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, 
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR 
+ * IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * 
  */
+
 package ru.boomik.infles;
 
 import java.io.File;
@@ -60,20 +64,17 @@ public class InflesActivity extends Activity {
 			"webkit"
 			};
 	
-	//!!SETTING:
-	/*
+	/* SETTING:
+	 * 
 	 * COPY_DIR - directory on SD card for copy files 
 	 *  example: 
 	 * "Infles" (Copy files to /sdcard/Infles/)
 	 * "Infles/subdir" (Copy files to /sdcard/Infles/subdir/)
-	 */
-	/*
+	 * 
 	 * UNZIP - unzipped *.zip files?
-	 */
-	/*
+	 * 	 
 	 * DEL_ZIP - delete all zip files;
-	 */
-	/*
+	 * 	 
 	 * SHOW_BUTTON - show 3 buttons after "RUN!" button
 	 */
 	boolean SHOW_BUTTON = true; 
@@ -196,28 +197,21 @@ public class InflesActivity extends Activity {
     
     protected Dialog onCreateDialog(int id) {
         super.onCreateDialog(id);
-     //   LinearLayout confirmView = new LinearLayout(this);
         switch(id) {
         case ABOUT:
-        	//Context mContext = getApplicationContext();
         	Dialog dialog = new Dialog(this);
         	dialog.setContentView(R.layout.about);
         	dialog.setTitle("About");	
         	TextView text = (TextView) dialog.findViewById(R.id.text);
-        	text.setText(Html.fromHtml("Infles - programm from copy files from app to SD card.<br /><br />Author: Kirill \"BOOM\" Ashikhmin<br />Email: <a href=\"mailto:boom.vrn@gmail.com\">boom.vrn@gmail.com</a><br />Site: <a href=\"http://boomik.ru\">http://boomik.ru</a><br />Source: <a href=\"http://code.google.com/p/infles/\">http://code.google.com/p/infles/</a><br /><br />License: GNU GPL v2."));
+        	text.setText(Html.fromHtml("Infles - programm from copy files from app to SD card.<br /><br />Author: Kirill \"BOOM\" Ashikhmin<br />Email: <a href=\"mailto:boom.vrn@gmail.com\">boom.vrn@gmail.com</a><br />Site: <a href=\"http://boomik.ru\">http://boomik.ru</a><br />Source: <a href=\"http://code.google.com/p/infles/\">http://code.google.com/p/infles/</a><br /><br />License: MIT."));
         	text.setMovementMethod(LinkMovementMethod.getInstance());
         	ImageView image = (ImageView) dialog.findViewById(R.id.image);
         	image.setImageResource(R.drawable.icon);
         	return dialog;
         case PROGRESS:
-            // Диалог загрузки
-        	
-        	//ProgressDialog mProgressDialog = ProgressDialog.show(InflesActivity.this.getApplicationContext(), "Please, wait...", "Copying files...", true);
-            
         	ProgressDialog dialogPr = new ProgressDialog(this);
             dialogPr.setTitle("Please, wait...");
             dialogPr.setMessage("Copying files...");
-            // без процентов
             dialogPr.setIndeterminate(true);
             return dialogPr;
         default:
